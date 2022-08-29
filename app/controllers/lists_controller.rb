ceeeb89e956b4aa00ebcbf83cf0c6330dbe024da
jin1210 <jinj86530@gmail.com>
2022-08-29 12:03:00 +0000

    [update]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,12 @@ class ListsController < ApplicationController
 
   def create
     list = List.new(list_params)
-    list.save
-    redirec_to list_path(list.id)
+    if list.save
+     redirec_to list_path(list.id)
+    else
+     render :new
+    end
+    
   end
   
   def index
